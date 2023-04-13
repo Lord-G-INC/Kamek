409e1021d21919c641f3f8306d6baafbeaa0ae00
Galaxy Master <76506954+MasterofGalaxies@users.noreply.github.com>
2023-04-12 20:53:30 -0400

    Add libkamek.h

---
+++ libkamek.h
@@ -0,0 +1,28 @@
+#ifndef __LIBKAMEK_H
+#define __LIBKAMEK_H
+
+enum patch_type {
+	BIN,
+	XML,
+	INI,
+	DOL,
+};
+
+struct ptr_info {
+	/*
+	 * Points to binary data (unsigned char *) for BIN
+	 * and DOL, string array (char **) for XML and INI
+	 */
+	void *ptr;
+
+	/*
+	 * Size of data for BIN and DOL, number of
+	 * strings in array for XML and INI
+	 */
+	size_t size;
+};
+
+extern void kamek_init(void);
+extern struct ptr_info kamek_createpatch(char **patches, int patch_count, char *game, enum patch_type patch_type, unsigned int base_address);
+
+#endif
